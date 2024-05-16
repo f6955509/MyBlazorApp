b6f6a64279d10da2a43eb4e40820c192cd8cb3d9
f6955509 <37483039+f6955509@users.noreply.github.com>
2024-05-16 12:34:14 +0100

    Inject MyNoteService

--- BlazorDemoApp/Program.cs
+++ BlazorDemoApp/Program.cs
@@ -1,4 +1,5 @@
 using BlazorDemoApp;
+using BlazorDemoApp.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<IMyNoteService, MyNoteService>();
 
 await builder.Build().RunAsync();
